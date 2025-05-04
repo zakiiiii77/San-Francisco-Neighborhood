b97e156670df2185d71cbf7e157eb3f6038400bc
zakiiiii77 <125500686+zakiiiii77@users.noreply.github.com>
2025-05-04 20:11:27 +0300

    website base

--- sfdorms/roommates/views.py
+++ sfdorms/roommates/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage(request):
+    return render(request, 'roommates/index.html')
+
+def about(request):
+    return render(request, 'roommates/about.html')
+
+def contact(request):
+    return render(request, 'roommates/contact.html')
